0598992b70998528373650269d98ed0e86ec88c1
k-donn <mitchell@kevinandjudy.com>
2020-04-04 20:16:21 -0400

    Added styling and moved funcs around

--- vec.py
+++ vec.py
@@ -1,57 +1,90 @@
 import math
+from typing import Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
-from matplotlib.text import Text
 from matplotlib.axes import Axes
+from matplotlib.axis import XAxis, YAxis
 from matplotlib.figure import Figure
 from matplotlib.quiver import Quiver
+from matplotlib.text import Text
+from matplotlib.ticker import MultipleLocator
 
 # TODO
-# Add graph of cross-product
-# Fix fargs dummy issue
+# Add graph of cross-product as a function of theta
+# Make graph sizing generated?
 
 MAG_1 = 4
-MAG_2 = 4
+MAG_2 = 3
 
 
 def format_plt():
     plt.style.use("ggplot")
 
 
-def main():
-    thetas = {}
-    thetas["theta_1"] = 0
-    thetas["theta_2"] = 0
+def format_plt_1(plt_1: Axes):
+    x_axis: XAxis = plt_1.get_xaxis()
+    y_axis: YAxis = plt_1.get_yaxis()
 
+    y_min_loc: MultipleLocator = MultipleLocator(1)
+    y_axis.set_minor_locator(y_min_loc)
+
+    x_min_loc: MultipleLocator = MultipleLocator(1)
+    x_axis.set_minor_locator(x_min_loc)
+
+    plt_1.grid(axis="y", which="major", lw=1.5)
+    plt_1.grid(axis="y", which="minor", lw=0.5)
+
+    plt_1.grid(axis="x", which="major", lw=1.5)
+    plt_1.grid(axis="x", which="minor", lw=0.5)
+
+    plt_1.set_xlabel("X")
+    plt_1.set_ylabel("Y")
+
+    plt_1.set_ylim([-5, 5])
+    plt_1.set_xlim([-5, 5])
+
+    plt_1.set_title("Vectors plotted on grid.")
+
+
+def plot_quiver(axes: Axes) -> Tuple[Quiver, Text]:
     x_1, y_1 = (MAG_1 *
-                math.cos(thetas["theta_1"]), MAG_1 *
-                math.sin(thetas["theta_1"]))
+                math.cos(0), MAG_1 *
+                math.sin(0))
     x_2, y_2 = (MAG_2 *
-                math.cos(thetas["theta_2"]), MAG_2 *
-                math.sin(thetas["theta_2"]))
+                math.cos(0), MAG_2 *
+                math.sin(0))
 
     # Remeber, U is all of the x-components not the first vector. V for Y
     vecs: np.ndarray = np.array([[0, 0, x_1, y_1], [0, 0, x_2, y_2]])
-    X, Y, U, V = zip(*vecs)
+    x_origins, y_origins, x_comps, y_comps = zip(*vecs)
 
-    format_plt()
-
-    fig: Figure = plt.figure()
-    ax: Axes = fig.add_subplot(111)
-
-    arrows: Quiver = ax.quiver(
-        X,
-        Y,
-        U,
-        V,
+    arrows: Quiver = axes.quiver(
+        x_origins,
+        y_origins,
+        x_comps,
+        y_comps,
         angles='xy',
         scale_units='xy',
         scale=1,
         color=("r", "b"))
 
-    info: Text = ax.text(1.9, 3, f"Cross Product: 0")
+    info: Text = axes.text(1.9, 3, f"Cross Product: 0")
+
+    return (arrows, info)
+
+
+def main():
+
+    format_plt()
+
+    fig: Figure = plt.figure()
+    plt_1: Axes = fig.add_subplot(111)
+
+    format_plt_1(plt_1)
+
+    (arrows, info) = plot_quiver(plt_1)
 
     anim = FuncAnimation(
         fig,
@@ -60,17 +93,13 @@ def main():
         interval=1000 / 30,
         blit=False,
         repeat=False,
-        fargs=(arrows, ax, info))
-
-    ax.set_xlim([-5, 5])
-    ax.set_ylim([-5, 5])
+        fargs=(arrows, plt_1, info))
 
     # plt.draw()
     plt.show()
 
 
-def animate(theta: float, arrows: Quiver, ax: Axes, info: Text):
-
+def update_plt_1(plt_1: Axes, arrows: Quiver, info: Text, theta: float):
     x_1, y_1 = (MAG_1 *
                 math.cos(theta), MAG_1 *
                 math.sin(theta))
@@ -79,13 +108,17 @@ def animate(theta: float, arrows: Quiver, ax: Axes, info: Text):
                 math.sin(-theta))
 
     vecs: np.ndarray = np.array([[x_1, y_1], [x_2, y_2]])
-    U, V = zip(*vecs)
+    x_comps, y_comps = zip(*vecs)
 
     cross_prod = np.cross(*vecs)
 
     info.set_text(f"Cross product: {cross_prod:>6.2f}")
 
-    arrows.set_UVC(U, V)
+    arrows.set_UVC(x_comps, y_comps)
+
+
+def animate(theta: float, arrows: Quiver, plt_1: Axes, info: Text):
+    update_plt_1(plt_1, arrows, info, theta)
 
 
 if __name__ == "__main__":
