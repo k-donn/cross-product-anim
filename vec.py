43abb4f68abb6d82ed6ffc27bef05f363d5e0903
k-donn <mitchell@kevinandjudy.com>
2020-06-03 15:47:20 -0400

    Added matplot-fmt-pi package

--- vec.py
+++ vec.py
@@ -15,6 +15,7 @@ from typing import Any, Callable, List, Optional, Tuple, TypedDict
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from matplot_fmt_pi import MultiplePi
 from matplotlib.animation import FuncAnimation
 from matplotlib.artist import Artist
 from matplotlib.axes import Axes
@@ -25,10 +26,9 @@ from matplotlib.lines import Line2D
 from matplotlib.patches import Patch
 from matplotlib.quiver import Quiver
 from matplotlib.text import Text
-from matplotlib.ticker import FuncFormatter, MultipleLocator
+from matplotlib.ticker import MultipleLocator
 
 # TODO
-# Extract formatting class to its own PyPi package?
 # Command line args for vectors' magnitudes?
 # Make graph sizing generated?
 # Move main() initialization to init_anim()
@@ -48,138 +48,6 @@ class LineDict(TypedDict):
     line: Line2D
     x_data: List[float]
     y_data: List[float]
-
-
-class MultiplePi:
-    r"""
-    Handle formatting of numbers as multiples of pi.
-
-    Attributes
-    ----------
-    denominator : `int`
-        The denominator of the multiples desired.
-
-    base : `float`, optional
-        Number to find multiples of, by default math.pi
-
-    symbol : `str`, optional
-        Symbol to place in string of multiple, by default r"\pi
-
-    """
-
-    def __init__(self, denominator: int, base: float = math.pi,
-                 symbol: str = r"\pi"):
-        """Initialize self."""
-        self.denominator = denominator
-        self.base = base
-        self.symbol = symbol
-
-    def locator(self) -> MultipleLocator:
-        """Return the locator with ticks at multiples of base via denominator.
-
-        Returns
-        -------
-        `MultipleLocator`
-            The object used to space the ticks
-        """
-        return MultipleLocator(self.base / self.denominator)
-
-    def formatter(self) -> FuncFormatter:
-        """Return the formatter for multiple ticks.
-
-        Returns
-        -------
-        `FuncFormatter`
-            Used to insert the symbol into the multiples
-        """
-        return FuncFormatter(self._make_formatter())
-
-    def _make_formatter(self) -> Callable[[float, Any], str]:
-        """Return the function used by the FuncFormatter instance.
-
-        Returns
-        -------
-        `Callable[[float, Any], str]`
-            Accpes a value and a position parameter and transforms them
-        """
-        def _fmt(theta, _) -> str:
-            """Transform the passed value into the proper representation of the multiple.
-
-            Parameters
-            ----------
-            theta : `float`
-                The angle in radians to be transformed
-            _ : `int`
-                Index of the tick on the axis
-
-            Returns
-            -------
-            `str`
-                The final label to be shown on the axis
-            """
-            denom = self.denominator
-            # Find raw numerator by finding how many (denom)s are in (theta)
-            # eg. How many pi/4 are in 1.5pi? (6)
-            numer = np.int(np.rint(denom * theta / self.base))
-
-            # Simplify the raw (numer) to lowest eg. 6/4 to 3/2
-            com = self._gcd(numer, denom)
-
-            # Simplify by dividing raw (numer) and (denom) by GCD
-            (numer, denom) = (int(numer / com), int(denom / com))
-
-            # If an integer of base
-            if denom == 1:
-                if numer == 0:
-                    return r"$0$"
-
-                # When numer simplifies to be +/- 1
-                if numer in (-1, 1):
-                    return r"${0}{1}$".format(
-                        "-" if numer == -1 else "", self.symbol)
-
-                # Just (multiple) * (base)
-                return r"${0}{1}$".format(numer, self.symbol)
-
-            # between inetegers of base
-            # When numer simplifies to be +/- 1
-            if numer in (-1, 1):
-                return r"$\frac{{{0}{2}}}{{{1}}}$".format(
-                    "-" if numer == -1 else "", denom, self.symbol)
-
-            # Simplified ((numer) * (base))/(denom)
-            return r"$\frac{{{0}{2}}}{{{1}}}$".format(
-                numer, denom, self.symbol)
-
-        return _fmt
-
-    @staticmethod
-    def _gcd(int_1: float, int_2: float) -> float:
-        """Return the Greatest Common Divisor of int_1 and int_2.
-
-        AKA, greatest common factor or greatest common measure.
-
-        Parameters
-        ----------
-        int_1 : `float`
-            First integer
-
-        int_2 : `float`
-            Second integer
-
-        Returns
-        -------
-        `float`
-            The largest positive integer that divides each of the integers
-
-        Raises
-        ------
-        `ValueError`
-            The greatest common divisor can only be of positive numbers
-        """
-        while int_2:
-            int_1, int_2 = int_2, int_1 % int_2
-        return int_1
 
 
 def calc_vector_comps(theta_1: float, theta_2: float) -> VectorPair:
@@ -263,13 +131,13 @@ def format_plt_2(plt_2: Axes) -> None:
         ylabel="Cross Product",
         title="Cross Product Theta Relationship")
 
-    pi_formatter: MultiplePi = MultiplePi(4)
+    maj_manager = MultiplePi(denominator=4)
+    min_manager = MultiplePi(denominator=12)
 
-    x_axis.set_major_locator(pi_formatter.locator())
-    x_axis.set_major_formatter(pi_formatter.formatter())
+    x_axis.set_major_locator(maj_manager.locator())
+    x_axis.set_major_formatter(maj_manager.formatter())
 
-    x_min_loc: MultipleLocator = MultipleLocator(math.pi / 12)
-    x_axis.set_minor_locator(x_min_loc)
+    x_axis.set_minor_locator(min_manager.locator())
 
     plt_2.set_ylim([-17, 17])
     plt_2.set_xlim([- math.pi / 8, math.pi + (math.pi / 8)])
