1ae38055039bfcc5326b5b177193c8d4d4d4cf0c
k-donn <mitchell@kevinandjudy.com>
2023-05-28 17:52:51 -0400

    Added command line magnitude parameters

--- vec.py
+++ vec.py
@@ -1,21 +1,30 @@
 """
-Show an animation of cross products.
+Show an animation of vector cross products.
 
-usage: python3.8 vec.py
+usage: python3.8 vec.py [-h] mag1 mag2
 
 One plot shows the vectors rotating about the origin and displays their cross product.
 Adjacent to that, a plot displays the cross product as a function of
 theta (absolute value of each vectors' angles above/below the X-axis).
 
 Each of the vectors' magnitudes remain constant and the only transformation is rotation.
+
+positional arguments:
+  mag1        Magnitude of vector 1
+  mag2        Magnitude of vector 2
+
+optional arguments:
+  -h, --help  show this help message and exit
+
 """
+from argparse import ArgumentParser
 import math
 from typing import Any, Callable, List, Optional, Tuple, TypedDict
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-from matplot_fmt_pi import MultiplePi
+from matplot_fmt_pi.ticker import MultiplePi
 from matplotlib.animation import FuncAnimation
 from matplotlib.artist import Artist
 from matplotlib.axes import Axes
@@ -33,8 +42,6 @@ from matplotlib.ticker import MultipleLocator
 # Make graph sizing generated?
 # Move main() initialization to init_anim()
 
-MAG_1 = 4
-MAG_2 = 3
 
 # The components of a vector
 Vector = Tuple[float, float]
@@ -50,7 +57,8 @@ class LineDict(TypedDict):
     y_data: List[float]
 
 
-def calc_vector_comps(theta_1: float, theta_2: float) -> VectorPair:
+def calc_vector_comps(theta_1: float, theta_2: float,
+                      mag1: float, mag2: float) -> VectorPair:
     """Calculate the components of two vectors with constant magnitude.
 
     Parameters
@@ -65,11 +73,11 @@ def calc_vector_comps(theta_1: float, theta_2: float) -> VectorPair:
     `VectorPair`
         The components of the vectors
     """
-    x_1, y_1 = (MAG_1 *
-                math.cos(theta_1), MAG_1 *
+    x_1, y_1 = (mag1 *
+                math.cos(theta_1), mag1 *
                 math.sin(theta_1))
-    x_2, y_2 = (MAG_2 *
-                math.cos(theta_2), MAG_2 *
+    x_2, y_2 = (mag2 *
+                math.cos(theta_2), mag2 *
                 math.sin(theta_2))
 
     return ((x_1, y_1), (x_2, y_2))
@@ -86,7 +94,7 @@ def format_plt() -> None:
     plt.tight_layout()
 
 
-def format_plt_1(plt_1: Axes) -> None:
+def format_plt_1(plt_1: Axes, mag1: float, mag2: float) -> None:
     """Format the vector plot after it has been rendered.
 
     Parameters
@@ -102,22 +110,22 @@ def format_plt_1(plt_1: Axes) -> None:
     x_axis: XAxis = plt_1.get_xaxis()
     y_axis: YAxis = plt_1.get_yaxis()
 
-    y_min_loc: MultipleLocator = MultipleLocator(1)
-    y_axis.set_minor_locator(y_min_loc)
+    # y_min_loc: MultipleLocator = MultipleLocator(1)
+    # y_axis.set_minor_locator(y_min_loc)
 
-    x_min_loc: MultipleLocator = MultipleLocator(1)
-    x_axis.set_minor_locator(x_min_loc)
+    # x_min_loc: MultipleLocator = MultipleLocator(1)
+    # x_axis.set_minor_locator(x_min_loc)
 
     plt_1.grid(axis="both", which="major", lw=1.5)
     plt_1.grid(axis="both", which="minor", lw=0.5)
 
     plt_1.set(xlabel="$X$", ylabel="$Y$", title="Vectors plotted on grid")
 
-    plt_1.set_ylim([-5, 5])
-    plt_1.set_xlim([-5, 5])
+    plt_1.set_xlim([-max(mag1, mag2) - 1, max(mag1, mag2) + 1])
+    plt_1.set_ylim([-max(mag1, mag2) - 1, max(mag1, mag2) + 1])
 
 
-def format_plt_2(plt_2: Axes) -> None:
+def format_plt_2(plt_2: Axes, mag1: float, mag2: float) -> None:
     """Format the cross product plot after it has been rendered.
 
     Parameters
@@ -139,11 +147,18 @@ def format_plt_2(plt_2: Axes) -> None:
 
     x_axis.set_minor_locator(min_manager.locator())
 
-    plt_2.set_ylim([-17, 17])
+    ((x_1, y_1), (x_2, y_2)) = calc_vector_comps(
+        math.pi / 4, -math.pi / 4, mag1, mag2)
+    vecs: np.ndarray = np.array([[x_1, y_1], [x_2, y_2]])
+
+    # gets maximum cross product to know hot to set axes
+    bound = abs(np.cross(*vecs))
+    bound = bound + bound / 10
+    plt_2.set_ylim([-bound, bound])
     plt_2.set_xlim([- math.pi / 8, math.pi + (math.pi / 8)])
 
 
-def plot_quiver(axes: Axes) -> Tuple[Quiver, Text]:
+def plot_quiver(axes: Axes, mag1: float, mag2: float) -> Tuple[Quiver, Text]:
     """Plot the vector arrows on the plot.
 
     Parameters
@@ -156,7 +171,7 @@ def plot_quiver(axes: Axes) -> Tuple[Quiver, Text]:
     `Tuple[Quiver, Text]`
         The quiver objects as well as the cross product text
     """
-    ((x_1, y_1), (x_2, y_2)) = calc_vector_comps(0, 0)
+    ((x_1, y_1), (x_2, y_2)) = calc_vector_comps(0, 0, mag1, mag2)
 
     vecs: np.ndarray = np.array([[0, 0, x_1, y_1], [0, 0, x_2, y_2]])
     x_origins, y_origins, x_comps, y_comps = zip(*vecs)
@@ -171,7 +186,7 @@ def plot_quiver(axes: Axes) -> Tuple[Quiver, Text]:
         scale=1,
         color=("r", "g"))
 
-    info: Text = axes.text(-4, 4, f"Cross Product: 0")
+    info: Text = axes.text(0.1, 0.9, f"Cross Product: 0", transform=axes.transAxes)
 
     return (arrows, info)
 
@@ -228,7 +243,7 @@ def init_anim_factory(arrows: Quiver, info: Text,
 
 
 def update_plt_1(arrows: Quiver,
-                 info: Text, theta: float) -> float:
+                 info: Text, theta: float, mag1: float, mag2: float) -> float:
     """Update the vector plot with new angles and re-calculate cross product.
 
     Parameters
@@ -247,7 +262,7 @@ def update_plt_1(arrows: Quiver,
     `float`
         The cross product at this angle
     """
-    ((x_1, y_1), (x_2, y_2)) = calc_vector_comps(theta, -theta)
+    ((x_1, y_1), (x_2, y_2)) = calc_vector_comps(theta, -theta, mag1, mag2)
     vecs: np.ndarray = np.array([[x_1, y_1], [x_2, y_2]])
     x_comps, y_comps = zip(*vecs)
 
@@ -289,7 +304,7 @@ def update_plt_2(line_dict: LineDict, theta: float,
 
 
 def animate(theta: float, arrows: Quiver, info: Text,
-            line_dict: LineDict) -> List[Artist]:
+            line_dict: LineDict, mag1: float, mag2: float) -> List[Artist]:
     """Update all plots with new data.
 
     Parameters
@@ -311,7 +326,7 @@ def animate(theta: float, arrows: Quiver, info: Text,
     `List[Artist]`
         The updated artists
     """
-    cross_prod = update_plt_1(arrows, info, theta)
+    cross_prod = update_plt_1(arrows, info, theta, mag1, mag2)
 
     update_plt_2(line_dict, theta, cross_prod)
 
@@ -320,16 +335,25 @@ def animate(theta: float, arrows: Quiver, info: Text,
 
 def main() -> None:
     """Run all executable code."""
+    parser = ArgumentParser(
+        prog="python3.8 vec.py",
+        description="Show an animation of vector cross products.",
+    )
+    parser.add_argument("mag1", type=float, help="Magnitude of vector 1")
+    parser.add_argument("mag2", type=float, help="Magnitude of vector 2")
+
+    args = parser.parse_args()
+
     setup_plt()
 
     fig: Figure = plt.figure(figsize=(9, 4.5), dpi=140)
     plt_1: Axes = fig.add_subplot(121)
     plt_2: Axes = fig.add_subplot(122)
 
-    format_plt_1(plt_1)
-    format_plt_2(plt_2)
+    format_plt_1(plt_1, args.mag1, args.mag2)
+    format_plt_2(plt_2, args.mag1, args.mag2)
 
-    (arrows, info) = plot_quiver(plt_1)
+    (arrows, info) = plot_quiver(plt_1, args.mag1, args.mag2)
     line = plot_cross_prod_line(plt_2)
 
     # Include line_dict and info under plt_x in future
@@ -343,7 +367,7 @@ def main() -> None:
         fig,
         animate,
         init_func=init_anim_factory(arrows, info, line),
-        fargs=(arrows, info, line_dict),
+        fargs=(arrows, info, line_dict, args.mag1, args.mag2),
         frames=np.linspace(0, math.pi, 128),
         interval=1000 / 30,
         repeat=False)
